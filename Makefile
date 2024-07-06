76e6ef76cce687cf755b883d1850045551fd6ebc
elutins <evandlutins@gmail.com>
2024-07-06 18:57:17 -0500

    initial commit

---
+++ Makefile
@@ -0,0 +1,4 @@
+install:
+	conda env create -f environment/conda_env.yml &&\
+	conda activate .flask-ml-cloud-serverless-demo
+
---
+++ app.py
@@ -0,0 +1,71 @@
+from flask import Flask, request, jsonify
+from flask.logging import create_logger
+import logging
+import traceback
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+
+
+import pandas as pd
+
+import pickle
+
+app = Flask(__name__)
+LOG = create_logger(app)
+LOG.setLevel(logging.INFO)
+
+MODEL_FEATURES = [
+    "plusMinus",
+    "Position",
+    "G",
+    "A1",
+    "GP",
+    "Grit",
+    "TOI%",
+    "xGA",
+    "xGF",
+]
+ID_FEATURES = ["First Name", "Last Name", "Team"]
+
+
+@app.route("/")
+def home():
+    html = "<h3>Sklearn Random Forest Predict NHL Player Salary: Hosted by Azure Pipelines</h3>"
+    return html.format(format)
+
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    """Performs predictions on static test data."""
+
+    try:
+        with open("simple_pipeline_clf.pkl", "rb") as f:
+            clf = pickle.load(f)
+
+    except Exception as e:
+        LOG.error(f"Error loading model: {e}")
+        LOG.error(f"Exception traceback: {traceback.format_exc()}")
+        return "Model was not loaded"
+
+    # ct = ColumnTransformer(
+    #     transformers=[("cat", OneHotEncoder(handle_unknown="ignore"), ["Position"])],
+    #     remainder="passthrough",
+    # )
+    # pipe = Pipeline([("categorical_encode", ct), ("clf", clf)])
+
+    json_payload = request.json
+    sample_prediction_records = pd.DataFrame(json_payload)
+
+    preds = clf.predict(sample_prediction_records[MODEL_FEATURES])
+
+    # return render_template(
+    #     "view.html",
+    #     tables=[sample_prediction_records[ID_FEATURES + ["preds"]].to_html()],
+    #     titles="RF Salary Predictions",
+    # )
+    return jsonify({"predictions": list(preds)})
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
